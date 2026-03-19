890014fe58730727603e92df771a9ffd21681ba2
Mitsuru Takahashi <mitsuru.takahashi@live.jp>
2026-03-19 11:49:37 +0000

    (main) Add PrimeNG

---
+++ src/app/ui/button/button.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-button',
+  imports: [ButtonModule],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class Button {}
