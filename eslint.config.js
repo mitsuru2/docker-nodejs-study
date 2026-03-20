d991a11aab86acfad374a2f53c60ea1126a7a199
Mitsuru Takahashi <mitsuru.takahashi@live.jp>
2026-03-20 12:01:37 +0000

    (main) Enable Lint checking.

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      // 通常のforスタイルが理解しやすい場合もあるため強制はしない。
+      '@typescript-eslint/prefer-for-of': 'off',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {
+      // Prime-NGのコンポーネントスタイルと競合のため。
+      '@angular-eslint/template/elements-content': 'off',
+      // 旧Angularディレクティブによるテンプレート制御を禁止
+      '@angular-eslint/template/prefer-control-flow': 'error',
+    },
+  },
+]);
